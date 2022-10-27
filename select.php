e55b6a0099a8b02a574ba0ac2088e8f53114d807
JosepMariaMaench <98151393+JosepMariaMaench@users.noreply.github.com>
2022-10-27 18:04:34 +0200

    Tarea2_a
    Executar el create table

--- select.php
+++ select.php
@@ -1,2 +1,28 @@
 <?php
 
+require_once 'server.php';
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "php";
+
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if ($mensaje = "" && $nomError == "" && $cognomError == "" && $emailError == "" && $contraError == "" && $confcontraError  == "" && $datacaducaError == "" && $numtargError == "" && $codsegurError == "") {
+        $sql = "INSERT INTO usuaris (nom, cognom, email, contrassenya, direccio, numtargeta, datacaduca, codiseguretat) 
+        VALUES ('$name', '$lastname', '$email', '$contra', '$direccio', '$numtarg', '$datcaduca', '$codsegur')";
+        if ($conn->query($sql) === true) {
+            echo "New record created successfully";
+            $_SESSION['id'] = $conn->insert_id;
+        } else {
+            echo "Error: " . $sql . "<br>" . $conn->error;
+        }
+    }
+}
+$conn->close();
